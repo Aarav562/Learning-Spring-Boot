1c31773d1bb521fccb44db3c27dc4f5cbe6268c1
Aarav D.C <dc.aarav@gmail.com>
2025-03-12 18:59:07 +0545

    First Project

--- Learning/src/main/java/com/Learn/Learning/LearningApplication.java
+++ Learning/src/main/java/com/Learn/Learning/LearningApplication.java
@@ -3,11 +3,9 @@ package com.Learn.Learning;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.Learn.Learning")
 public class LearningApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LearningApplication.class, args);
 	}
-
 }
