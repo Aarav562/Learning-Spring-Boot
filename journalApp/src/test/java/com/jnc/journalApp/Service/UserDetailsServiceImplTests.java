8fa09a33b78ffaf1f559ac22012b7e6b4e0288be
Aarav D.C <dc.aarav@gmail.com>
2025-03-28 13:42:13 +0545

    Mockito

--- journalApp/src/test/java/com/jnc/journalApp/Service/UserDetailsServiceImplTests.java
+++ journalApp/src/test/java/com/jnc/journalApp/Service/UserDetailsServiceImplTests.java
@@ -1,5 +1,36 @@
 package com.jnc.journalApp.Service;
 
+import com.jnc.journalApp.Entity.User;
+import com.jnc.journalApp.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+import org.springframework.boot.test.mock.mockito.*;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+
 public class UserDetailsServiceImplTests{
 
+    @InjectMocks
+    private UserDetailsServiceImpl userDetailsService;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Test
+    void loadUsernameByTest(){
+        when(userRepository.findByUsername(ArgumentMatchers.anyString())).thenReturn(User.builder().username("Ram").password("inkink").roles(new ArrayList<>()).build());
+        UserDetails user = userDetailsService.loadUserByUsername("Ram");
+        assertNotNull(user);
+    }
 }
