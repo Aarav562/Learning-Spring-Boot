4fb0bb5fe8dd570c729f565bdbb2f25a584a14f4
Aarav D.C <dc.aarav@gmail.com>
2025-03-31 22:53:40 +0545

    Profiles

--- journalApp/src/test/java/com/jnc/journalApp/Service/UserDetailsServiceImplTests.java
+++ journalApp/src/test/java/com/jnc/journalApp/Service/UserDetailsServiceImplTests.java
@@ -3,17 +3,19 @@ package com.jnc.journalApp.Service;
 import com.jnc.journalApp.Entity.User;
 import com.jnc.journalApp.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.*;
 import org.springframework.boot.test.mock.mockito.*;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
-
+@ActiveProfiles("dev")
 public class UserDetailsServiceImplTests{
 
     @InjectMocks
@@ -26,6 +28,7 @@ public class UserDetailsServiceImplTests{
 
     @Mock
     private UserRepository userRepository;
+
 
     @Test
     void loadUsernameByTest(){
--- journalApp/src/test/java/com/jnc/journalApp/Service/UserServiceTests.java
+++ journalApp/src/test/java/com/jnc/journalApp/Service/UserServiceTests.java
@@ -22,6 +22,7 @@ public class UserServiceTests {
     @Autowired
     private UserService userService;
 
+    @Disabled
     @ParameterizedTest
     @ValueSource(strings = {
             "Messi",
